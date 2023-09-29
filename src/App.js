43832d2356c611aae38b77d89de33d408cfe9fdd
ElGirasolMaestro <grisol225@gmail.com>
2023-09-28 21:51:45 -0500

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map  from './components/Map/Map';
+
+const App=() => {
+
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    );
+}
+
+export default App;
